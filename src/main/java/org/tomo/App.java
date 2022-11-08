e93692bcfa94d83f98610ff6435229c67de5aa67
Tomas Kundis <tomas.kundis@example.com>
2022-11-09 00:21:56 +0100

    REFACT: Hello World with Logback.

--- src/main/java/org/tomo/App.java
+++ src/main/java/org/tomo/App.java
@@ -1,10 +1,14 @@
 package org.tomo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Main application class.
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Logger logger = LoggerFactory.getLogger(App.class);
+    logger.info("Hello World!");
   }
 }
