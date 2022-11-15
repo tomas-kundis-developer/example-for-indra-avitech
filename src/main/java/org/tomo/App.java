2c4140668f23cb2594f6e89a1c6a2e9788b004f6
Tomas Kundis <tomas.kundis@example.com>
2022-11-15 04:19:08 +0100

    FEAT: Add application main method.

--- src/main/java/org/tomo/App.java
+++ src/main/java/org/tomo/App.java
@@ -2,13 +2,25 @@ package org.tomo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tomo.shared.exceptions.AppInitializationException;
 
 /**
  * Main application class.
  */
 public class App {
+  private static final Logger log = LoggerFactory.getLogger(App.class);
+
+  /**
+   * Application start.
+   */
   public static void main(String[] args) {
-    Logger logger = LoggerFactory.getLogger(App.class);
-    logger.info("Hello World!");
+    log.info("Application started.");
+
+    try {
+      AppInitialization.init();
+    } catch (AppInitializationException e) {
+      log.error("Exit with error code 1");
+      System.exit(1);
+    }
   }
 }
